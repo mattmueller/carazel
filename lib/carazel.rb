5d12c1c26a01407381a005e505009ecf7f6e4a0e
Matt Mueller <muellermr@gmail.com>
2010-11-01 07:35:41 -0500

    Working on post methods

--- lib/carazel.rb
+++ lib/carazel.rb
@@ -2,6 +2,8 @@ require 'rubygems'
 require 'httparty'
 require 'hashie'
 
+CLIENT_KEY = "Your API KEY"
+
 class Carazel
   include HTTParty
   base_uri 'dispatcher.carazel.com'
@@ -9,12 +11,12 @@ class Carazel
   #Find and show users
 
   def users
-    results = self.class.get('/users')
+    results = self.class.get('/users', :body => {:client_key => CLIENT_KEY})
     Hashie::Mash.new(results).users
   end
 
   def user(id)
-    results = self.class.get("/users/show?id=#{id}")
+    results = self.class.get("/users/show?id=#{id}", :body => {:client_key => CLIENT_KEY})
     Hashie::Mash.new(results).users rescue results
   end
 
@@ -22,21 +24,21 @@ class Carazel
 
   def rewards(type=nil)
     if type.nil?
-      results = self.class.get("/rewards/search")
+      results = self.class.get("/rewards/search", :body => {:client_key => CLIENT_KEY})
     else
-      results = self.class.get("/rewards/search?type=type")
+      results = self.class.get("/rewards/search?type=type", :body => {:client_key => CLIENT_KEY})
     end
     results.collect{|r| Hashie::Mash.new(r['reward'])}
   end
 
   def reward(id, reward_type)
-    results = self.class.get("/rewards/show?id=#{id}&reward_type=#{reward_type}")
+    results = self.class.get("/rewards/show?id=#{id}&reward_type=#{reward_type}", :body => {:client_key => CLIENT_KEY})
     Hashie::Mash.new(results.first['reward'])
   end
 
   def user_rewards(id, reward_type=nil)
     query = self.build_query_string([['id=',id],['reward_type=',reward_type]])
-    results = self.class.get("/users/rewards?#{query}").first
+    results = self.class.get("/users/rewards?#{query}", :body => {:client_key => CLIENT_KEY}).first
     Hashie::Mash.new(results).rewards rescue results
   end
 
@@ -44,35 +46,35 @@ class Carazel
   #Find and show places
 
   def place(id)
-    results = self.class.get("/places/show?id=#{id}")
+    results = self.class.get("/places/show?id=#{id}", :body => {:client_key => CLIENT_KEY})
     Hashie::Mash.new(results.first['place']) rescue results
   end
 
   def places(lat,lon,term=nil)
     query = self.build_query_string([['lat=',lat],['lon=',lon],['query=',term]])
-    results = self.class.get("/places/search?#{query}").collect{|r| Hashie::Mash.new(r['place'])}
+    results = self.class.get("/places/search?#{query}", :body => {:client_key => CLIENT_KEY}).collect{|r| Hashie::Mash.new(r['place'])}
   end
 
   #Find and show activities as well as a given user's activities
 
   def activities(type=nil, limit=nil)
     if type.nil? && limit.nil?
-      results = self.class.get("/activities/search")
+      results = self.class.get("/activities/search", :body => {:client_key => CLIENT_KEY})
     else
       query = self.build_query_string([['type=',type],['limit=',limit]])
-      results = self.class.get("/activities/search?#{query}")
+      results = self.class.get("/activities/search?#{query}", :body => {:client_key => CLIENT_KEY})
     end
     activities = results.collect{|r| Hashie::Mash.new(r['activity'])}
   end
 
   def activity(id)
-    results = self.class.get("/activities/show?id=#{id}")
+    results = self.class.get("/activities/show?id=#{id}", :body => {:client_key => CLIENT_KEY})
     Hashie::Mash.new(results.first['activity'])
   end
 
   def user_activities(id, since, activity_type=nil)
     query = self.build_query_string([['id=',id],['since=',since],['activity_type=',activity_type]])
-    results = self.class.get("/users/activities?#{query}")
+    results = self.class.get("/users/activities?#{query}", :body => {:client_key => CLIENT_KEY})
     Hashie::Mash.new(results[0]).activities rescue results
   end
 
@@ -80,17 +82,27 @@ class Carazel
 
   def checkins(id=nil, since=nil, offset=nil)
     query = self.build_query_string([['user_id=',id],['since=',since],['offset=',offset]])
-    results = self.class.get("/lbs_checkins/search?#{query}").collect{|r| Hashie::Mash.new(r['lbs_checkin'])}
+    results = self.class.get("/lbs_checkins/search?#{query}", :body => {:client_key => CLIENT_KEY}).collect{|r| Hashie::Mash.new(r['lbs_checkin'])}
   end
 
   def checkin(id)
-    results = self.class.get("/lbs_checkins/show?id=#{id}")
+    results = self.class.get("/lbs_checkins/show?id=#{id}", :body => {:client_key => CLIENT_KEY})
     Hashie::Mash.new(results.first['lbs_checkin'])
   end
 
   def user_checkins(id, since=nil, offset=nil)
     query = self.build_query_string([['user_id=',id],['since=',since],['offset=',offset]])
-    results = self.class.get("/lbs_checkins/search?#{query}").collect{|r| Hashie::Mash.new(r['lbs_checkin'])}
+    results = self.class.get("/lbs_checkins/search?#{query}", :body => {:client_key => CLIENT_KEY}).collect{|r| Hashie::Mash.new(r['lbs_checkin'])}
+  end
+
+  #Update a users info
+
+  def update_user(id, email=nil, mobile_number=nil, zip=nil)
+    result = self.class.post("/users/update", :body => {:client_key => CLIENT_KEY, :id => id, :email => email, :mobile_number => mobile_number, :zip => zip})
+  end
+
+  def create_user(email=nil, birth_date=nil, zip=nil, mobile_number=nil)
+    result = self.class.post("/users/create", :body => {:client_key => CLIENT_KEY, :email => email, :mobile_number => mobile_number, :zip => zip})
   end
 
   #Build query string for queries with multiple combinations of values
